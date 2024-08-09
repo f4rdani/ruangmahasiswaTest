f00d4deaafb9f2a2f9aa7dc048012858c1a6a72e
f4rdani <156650124+f4rdani@users.noreply.github.com>
2024-08-09 14:35:33 +0700

    Update pdfsuketmhs.php

--- views/pdf/pdfsuketmhs.php
+++ views/pdf/pdfsuketmhs.php
@@ -1,46 +1,221 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" dir="ltr" data-nav-layout="vertical" data-theme-mode="light" data-header-styles="light" data-menu-styles="light" data-toggled="close">
+
 <head>
-    <title>Surat Keterangan</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="<?= base_url() ?>/assets/images/logo_ubsi.png" type="image/x-icon">
+
+    <!-- Bootstrap Css -->
+    <link id="style" href="<?= base_url() ?>/assets/libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+    <!-- Custom styles -->
     <style>
         body {
             font-family: Arial, sans-serif;
+            margin: 0;
+            /* Remove default margin */
+            line-height: 1.2;
+            /* Adjust line height for tighter spacing */
         }
-        .header {
+
+        .container {
+            max-width: 900px;
+            margin: 0 auto;
+            padding: 20px;
+
+        }
+
+        .header img,
+        .footer img {
+            width: 100%;
+            height: auto;
+        }
+
+        .header,
+        .footer {
+            max-width: 900px;
+            margin: 0 auto;
+            padding: 1px;
+        }
+
+        .title {
+            font-size: 15px;
+            text-align: left;
+            margin: 10px 0;
+        }
+
+        .title2 {
+            font-size: 25px;
+            font-weight: bold;
             text-align: center;
+            margin: 10px 0;
+            padding-top: 20px;
         }
-        .content {
+
+        .nosurat {
+            font-size: 15px;
+            text-align: center;
+            margin: 10px 0;
+
+        }
+
+        .student-info {
+            margin-bottom: 10px;
+        }
+
+        .student-info p {
+            margin: 5px 0;
+        }
+
+        table {
+            width: 100%;
+            margin-bottom: 20px;
+        }
+
+        th,
+        td {
+            border: 1px solid #ffff;
+            padding: 5px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        .notes {
+            font-size: 12px;
             margin-top: 20px;
+        }
+
+        .biodata {
+            padding-left: 100px;
+            padding-right: 100px;
+            padding-top: 20px;
+        }
+
+        .ttd {
+            padding-left: 300px;
+            padding-top: 20px;
+        }
+
+        .info {
+            padding-top: 20px;
+            font-size: 12px;
+            margin: 0;
+            /* Remove default margin */
+            line-height: 0.2;
         }
     </style>
 </head>
+
 <body>
     <div class="header">
-        <h1>Surat Keterangan Mahasiswa</h1>
-        <h2>Universitas Bina Sarana Informatika</h2>
-        <hr>
+        <img src="<?= base_url() ?>/assets/images/header-ubsi.jpg" alt="Header UBSI">
     </div>
-    <div class="content">
-        <p>No.162337/4.01/BSI/VII/2024</p>
-        <p>Dengan ini Rektor Universitas Bina Sarana Informatika, menerangkan bahwa:</p>
-        <p>Nama : <?php echo $mhs->nm_mhs; ?></p>
-        <p>NIM : <?php echo $mhs->nim; ?></p>
-        <p>Tempat/Tgl. Lahir : <?php echo $mhs->t_lhr; ?>, <?php echo date('d F Y', strtotime($mhs->tgl_lhr)); ?></p>
-        <p>Alamat : <?php echo $mhs->alm; ?> <?php echo $mhs->no_rmh; ?> RT/RW <?php echo $mhs->rt_rw; ?> <?php echo $mhs->kecamatan; ?> <?php echo $mhs->kelurahan; ?> <?php echo $mhs->kota; ?> </p>
-        <p>Jenjang Pendidikan : Strata 1 <code>s</code></p>
-        <p>Program Studi : <?php echo $studi->prodi; ?></p>
-        <p>Semester : 6 <code>s</code></p>
-        <p>Tahun Akademik : 2023/2024 <code>s</code></p>
-        <p>Adalah benar Mahasiswa/i Universitas Bina Sarana Informatika Program Studi (<?php echo $studi->prodi; ?>) Program Strata 1 dan tercatat aktif pada Semester Ganjil 2023/2024. Demikian surat keterangan ini dibuat agar dipergunakan sebagaimana mestinya.</p>
-        <p>Jakarta, <?php echo date('d F Y'); ?></p>
-        <p>Rektor</p>
-        <p>Prof. Dr. Ir. Mochamad Wahyudi, M.Kom, MM, M.Pd, IPU, ASEAN Eng</p>
-        <p>Info :</p>
-        <ul>
-            <li>Pembuatan surat keterangan ini adalah sah, tidak perlu menyertakan Stempel dan tanda tangan</li>
-            <li>Pencetakan telah menggunakan login dengan Security Print Key : 0A5-88E-A8B-A2F-8DB</li>
-            <li>Surat Riset ini dicetak pada Tanggal <?php echo date('d-m-Y'); ?> Jam <?php echo date('h:i:s:A'); ?></li>
-        </ul>
+    <div class="container">
+
+
+        <div class="title">Surat Keterangan Mahasiswa Universitas Bina Sarana Informatika</div>
+
+        <div class="title2">SURAT KETERANGAN</div>
+        <div class="nosurat"> No.162337/4.01/BSI/VII/2024</div>
+
+        <div class="student-info">
+            <div class="div" style="padding-top: 30px;"> Dengan ini Rektor Universitas Bina Sarana Informatika, menerangkan bahwa:</div>
+            <div class="biodata">
+                <table class="table">
+                    <tr>
+                        <td style="width: 200px;">Nama </td>
+                        <td style="width: 5px;">:</td>
+                        <td> <?= $mhs->nm_mhs ?></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 200px;">Nim </td>
+                        <td style="width: 5px;">:</td>
+                        <td> <?= $mhs->nim ?></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 200px;">Alamat </td>
+                        <td style="width: 5px;">:</td>
+                        <td> <?= $mhs->alm ?></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 200px;">Jenjang Pendidikan </td>
+                        <td style="width: 5px;">:</td>
+                        <td> 17220245</td>
+                    </tr>
+                    <tr>
+                        <td style="width: 200px;">Progarm Studi </td>
+                        <td style="width: 5px;">:</td>
+                        <td> <?php echo $studi->nm_jrs; ?></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 200px;">Semester </td>
+                        <td style="width: 5px;">:</td>
+                        <td> 999</td>
+                    </tr>
+                    <?php
+                    function getAcademicYear()
+                    {
+                        date_default_timezone_set('Asia/Jakarta');
+                        $currentYear = date('Y');
+                        $startYear = $currentYear - 1;
+                        $endYear = $currentYear;
+                        return "$startYear-$endYear";
+                    }
+                    ?>
+
+                    <tr>
+                        <td style="width: 200px;">Tahun Akademik </td>
+                        <td style="width: 5px;">:</td>
+                        <td> <?php echo getAcademicYear(); ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class="student-info" style="padding-top: 20px;">
+            Adalah benar Mahasiswa/i Universitas Bina Sarana Informatika Program Studi (<?php echo $studi->nm_jrs; ?>) Program Strata 1
+            dan tercatat aktif pada Semester Ganjil <?php echo getAcademicYear(); ?>. Demikian surat keterangan ini dibuat agar dipergunakan sebagaimana
+            mestinya
+        </div>
+
+        <div class="ttd">
+            <p> Jakarta, <?php
+                            date_default_timezone_set('Asia/Jakarta');
+                            echo date('d F Y');
+                            ?></p>
+            <p>Rektor</p>
+
+            <p style="padding-top: 50px;"> Prof. Dr. Ir. Mochamad Wahyudi, M.Kom, MM, M.Pd, IPU, ASEAN En</p>
+
+
+        </div>
+
+        <div class="info">
+            <p style="font-weight: bold;">info :</p>
+            <p> 1. Pembuatan surat keterangan ini adalah sah, tidak perlu menyertakan Stempel dan tanda tangan</p>
+            <p> 2. Pencetakan telah menggunakan login dengan Security Print Key :</p>
+            <p> 3. Surat Riset ini di cetak pada Tanggal <?php echo $date; ?> ,Jam <?php echo $jam; ?></p>
+        </div>
+
+
+
     </div>
+    <div class="footer">
+        <img src="<?= base_url() ?>/assets/images/footer-ubsi.jpg" alt="Footer UBSI">
+    </div>
+
+    <!-- JavaScript libraries -->
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <script src="<?= base_url() ?>/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url() ?>/assets/js/main.js"></script>
 </body>
+
 </html>
