d63ab9c9bbf79c12690ea2df522f1d5db08f6202
Msann1 <156728902+Msann1@users.noreply.github.com>
2024-08-20 08:06:13 +0700

    Add files via upload

---
+++ views/mahasiswa/pengajuan_aktif_cuti_akademik.php
@@ -0,0 +1,31 @@
+<!-- Start::app-content -->
+<div class="main-content app-content">
+            <div class="container-fluid">
+
+                <header>
+         
+                </header>
+
+                <div class="container about-container">
+                     <!-- Start::row-3 -->
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card mt-7">
+                                <div class="card-body p-0 p-lg-5 text-dark">
+                                    <div class="statistics-info p-4">
+                                        <div class="row">
+                                            <div class="">
+                                                <div class="">
+                                                    <h5 class="leading-normal fw-normal mb-4 text-dark"><strong>Silahkan Melakukan Pembayaran Melalui Aplikasi E-KTM</strong></h5>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--End::row-3 -->
+            </div>
+        </div>
+        <!-- End::app-content -->
