4216be210155b77022d23e8e452e805dee70f3cd
Msann1 <156728902+Msann1@users.noreply.github.com>
2024-08-20 15:25:43 +0700

    Update jadwal_mengajar_dosen.php
    pake ajax

--- views/mahasiswa/jadwal_mengajar_dosen.php
+++ views/mahasiswa/jadwal_mengajar_dosen.php
@@ -35,8 +35,10 @@
                                         <td><?= $n; ?></td>
                                         <td><?= $p->kd_dosen ?></td>
                                         <td>
-                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                                    data-bs-target="#jadwalModal-<?php echo $p->kd_dosen; ?>">
+                                            <button type="button" class="btn btn-primary lihat-jadwal" 
+                                                    data-bs-toggle="modal"
+                                                    data-bs-target="#jadwalModal"
+                                                    data-kd_dosen="<?= $p->kd_dosen; ?>">
                                                 Lihat Jadwal
                                             </button>
                                         </td>
@@ -51,62 +53,48 @@
         </div>
         <!-- End::row-1 -->
 
-        <!-- Modal untuk setiap dosen -->
-        <?php foreach ($pertemuan as $p): ?>
-            <div class="modal fade" id="jadwalModal-<?php echo $p->kd_dosen; ?>" tabindex="-1"
-                aria-labelledby="jadwalModalLabel-<?php echo $p->kd_dosen; ?>" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-lg">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="jadwalModalLabel-<?php echo $p->kd_dosen; ?>">
-                                Jadwal Dosen: <?php echo $p->kd_dosen; ?>
-                            </h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered text-nowrap w-100">
-                                    <thead>
-                                        <tr>
-                                            <th>Kelas</th>
-                                            <th>Mata Kuliah</th>
-                                            <th>SKS</th>
-                                            <th>Hari</th>
-                                            <th>Jam</th>
-                                            <th>Ruangan</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        foreach ($pertemuan as $j):
-                                            if ($j->kd_dosen === $p->kd_dosen):
-                                        ?>
-                                        <tr>
-                                            <td><?= $j->kd_lokal ?></td>
-                                            <td><?= $j->nm_mtk ?></td>
-                                            <td><?= $j->sksajar ?></td>
-                                            <td><?= $j->hari_t ?></td>
-                                            <td><?= $j->jam_t ?></td>
-                                            <td><?= $j->no_ruang ?></td>
-                                        </tr>
-                                        <?php
-                                            endif;
-                                        endforeach;
-                                        ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                           
-                        </div>
+        <!-- Modal (Load Content via AJAX) -->
+        <div class="modal fade" id="jadwalModal" tabindex="-1" aria-labelledby="jadwalModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="jadwalModalLabel">Jadwal Dosen</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Content will be loaded here via AJAX -->
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
                     </div>
                 </div>
             </div>
-        <?php endforeach; ?>
+        </div>
         <!-- End Modal -->
 
     </div>
 </div>
 <!-- End::app-content -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+        // Event listener for the "Lihat Jadwal" button
+        $('.lihat-jadwal').on('click', function() {
+            var kd_dosen = $(this).data('kd_dosen');
+
+            // Use AJAX to load the modal content
+            $.ajax({
+                url: "<?php echo base_url('Mahasiswa/load_jadwal_dosen'); ?>",
+                type: 'POST',
+                data: { kd_dosen: kd_dosen },
+                success: function(data) {
+                    $('#jadwalModal .modal-body').html(data);
+                    $('#jadwalModalLabel').text('Jadwal Dosen: ' + kd_dosen);
+                },
+                error: function() {
+                    $('#jadwalModal .modal-body').html('<p>Error loading data.</p>');
+                }
+            });
+        });
+    });
+</script>
